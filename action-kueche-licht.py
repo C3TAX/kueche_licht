90b6f2e9437430fa6145dcbc3c7333b47192cd69
C3TAX <cetax@bk-s.de>
2019-11-24 14:57:58 +0100

    Create action-kueche-licht.py

---
+++ action-kueche-licht.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import configparser
+from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
+from hermes_python.ontology import *
+import io
+import websockets
+from websocket import create_connection
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+class SnipsConfigParser(configparser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, configparser.Error) as e:
+        return dict()
+        
+def msg_kueche_tisch_licht_an(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    
+    ws = create_connection("ws://192.168.178.102:8080")
+    ws.send("Update GA:00_0_005=1")
+    ws.close()
+    
+    if len(intentMessage.slots.snips_kueche) > 0:
+        house_room = intentMessage.snips_kueche.first().value # We extract the value from the slot "house_room"
+        result_sentence = "Tisch Licht in {} angeschaltet".format(str(snips_kueche))  # The response that will be said out loud by the TTS engine.
+    else:
+        result_sentence = "Kuechen Tisch Licht an"
+
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, result_sentence)
+    
+
+def msg_kueche_tisch_licht_aus(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    
+    ws = create_connection("ws://192.168.178.102:8080")
+    ws.send("Update GA:00_0_005=0")
+    ws.close()
+
+    if len(intentMessage.slots.snips_kueche) > 0:
+        house_room = intentMessage.slots.snips_kueche.first().value # We extract the value from the slot "house_room"
+        result_sentence = "Tisch Licht in {} ausgeschaltet".format(str(snips_kueche))  # The response that will be said out loud by the TTS engine.
+    else:
+        result_sentence = "Kuechen Tisch Licht aus"
+
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, result_sentence)
+    
+if __name__ == "__main__":
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent("cetax:Tisch_Licht_an", msg_kueche_tisch_licht_an)
+        h.subscribe_intent("cetax:Tisch_Licht_aus", msg_kueche_tisch_licht_aus)
+        h.start()
